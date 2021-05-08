8e6f685c0e82e8996ff041d5c91e827de212b242
Fil <mail@mail.ru>
2021-05-08 20:15:45 +0300

    code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict'
 
 
- let money = prompt("Ваш бюджет на месяц?", ''),
+ let money = +prompt("Ваш бюджет на месяц?", ''),
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
@@ -17,8 +17,8 @@ for (let i = 0; i < 2; i++){
 	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
 		b = prompt("Во сколько обойдется?", '');
 
-	if ( (typeof(a)) === 'string' && (typeof(a)) !=null && (typeof(b)) !=null
-		&& a != '' && b != '' && a.lenght < 50){
+	if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
+		&& a != '' && b != '' && a.length < 50){
 		console.log("done");
 		appData.expenses[a] = b;
 	} else {
