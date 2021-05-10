89f1ebbe6c37466c5b6d7d86f8d823522dd47c9d
Fil <mail@mail.ru>
2021-05-10 19:39:01 +0300

    Статья не обязательных расходов

--- js/script.js
+++ js/script.js
@@ -39,22 +39,30 @@ function chooseExpenses() {
 chooseExpenses();
 
 
-appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+function detectDayBudget(){
+	appData.moneyPerDay = (appData.budget / 30).toFixed();
+	alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб." )
+}
+detectDayBudget();
 
 
 
+function detectLevel() {
+	if(appData.moneyPerDay < 100) {
+		console.log("Минимальный уровень достатка")
+	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ) {
+		console.log("Средний уровень достатка");
+	} else if ( appData.moneyPerDay > 2000) {
+		console.log("Высокий уровень достатка");
+	} else {
+		console.log("Произошла ошибка");
+	} 
+}
+detectLevel();
 
-alert("Ежедневный бюджет: " + appData.moneyPerDay); 
 
-if(appData.moneyPerDay < 100) {
-	console.log("Минимальный уровень достатка")
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ) {
-	console.log("Средний уровень достатка");
-} else if ( appData.moneyPerDay > 2000) {
-	console.log("Высокий уровень достатка");
-} else {
-	console.log("Произошла ошибка");
-} 
+
 
 function checkSavings(){
 	if (appData.savings == true) {
@@ -65,5 +73,17 @@ function checkSavings(){
 		alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
 	}
 }
-
 checkSavings();
+
+
+
+function chooseOptExpenses() {                            
+
+    for (let i = 1; i <= 3; i++) {
+        let questionOptExpenses = prompt("Статья необязательных расходов?");
+        appData.optionalExpenses[i] = questionOptExpenses;
+        console.log(appData.optionalExpenses);
+    }
+
+}
+chooseOptExpenses();
